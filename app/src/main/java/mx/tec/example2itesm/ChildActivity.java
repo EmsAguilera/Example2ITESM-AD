86a71dedd2667f785625ed3ed38fa53a3d1b0011
A01351355DAW <A01351355@itesm.mx>
2020-08-30 23:11:43 -0500

    Agregar botón de regreso

--- app/src/main/java/mx/tec/example2itesm/ChildActivity.java
+++ app/src/main/java/mx/tec/example2itesm/ChildActivity.java
@@ -2,13 +2,28 @@ package mx.tec.example2itesm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChildActivity extends AppCompatActivity {
+
+    Button regresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child);
+
+        regresar = findViewById(R.id.regresar);
+
+        regresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(ChildActivity.this, MainActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
